beba35592117484ed69d6575f0fa0df42f4c8969
gandastik <gandastik@gmail.com>
2022-03-22 22:04:07 +0700

    add sign-in btn and style navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,36 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 import { GoChevronDown } from 'react-icons/go';
 
 const Navbar = () => {
+  const [isAuthen, setIsAuthen] = useState('false');
+
+  useEffect(() => {
+    setIsAuthen(true);
+  }, []);
+
   return (
-    <div className="bg-white-800 h-16 shadow-md w-full">
+    <div className="bg-white-800 h-16 shadow-md w-full mb-6">
       <div className="max-w-screen-2xl mx-auto h-full flex justify-between items-center">
         <div className="flex justify-center items-center gap-10">
-          <img src="" alt="" className="w-14 h-14 rounded-full border-2 border-red-200" />
+          <img
+            src="https://media.discordapp.net/attachments/903302214859255818/955820487735394364/Logo.png"
+            alt=""
+            className="h-16 w-16"
+          />
           <nav className="flex text-gray-800 font-medium gap-10">
-            <ul className="hover:cursor-pointer hover:text-gray-500">Schedule Generator</ul>
-            <ul className="hover:cursor-pointer hover:text-gray-500">Filter Subject</ul>
-            <ul className="hover:cursor-pointer hover:text-gray-500">Blog Review</ul>
+            <Link to="/generator">
+              <div className="hover:cursor-pointer hover:text-gray-500">Schedule Generator</div>
+            </Link>
+            <Link to="/filter">
+              <div className="hover:cursor-pointer hover:text-gray-500">Filter Subject</div>
+            </Link>
+            <Link to="/review">
+              <div className="hover:cursor-pointer hover:text-gray-500">Blog Review</div>
+            </Link>
           </nav>
         </div>
-        <div className="font-medium flex justify-center items-center gap-2">
-          <img src="" alt="" />
-          <div className="">Username</div>
-          <GoChevronDown className="hover:cursor-pointer text-gray-400" />
-        </div>
+        {isAuthen ? (
+          <div className="font-medium flex justify-center items-center gap-2">
+            {/* user's image and user's firstname goes here */}
+            <img
+              src="https://media.discordapp.net/attachments/904607198263509024/915881080706461696/ExodiatheForbiddenOne-TF04-JP-VG.jpg"
+              alt=""
+              className="h-10 w-10 border-2 border-orange-400 rounded-full shadow-md"
+            />
+            <div className="">Username</div>
+            <GoChevronDown className="hover:cursor-pointer text-gray-400" />
+          </div>
+        ) : (
+          <button className="bg-black py-2 px-6 rounded-full text-white font-medium hover:cursor-pointer hover:scale-105  transition-all ease-in-out ">
+            Sign In
+          </button>
+        )}
       </div>
-      {/* <div className="max-w-screen-2xl px-5 mx-auto h-full flex justify-between items-center">
-        <div className="flex justify-center gap-5">
-          <img
-            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.superdthailand.com%2Fnew-arrival%2Ftest.html&psig=AOvVaw3UOSQyKwXsEDXR8tTQi7QA&ust=1647842408732000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJDElMSB1PYCFQAAAAAdAAAAABAE"
-            alt=""
-          />
-          <div className="">Schedule Generator</div>
-          <div className="">Filter Subject</div>
-          <div className="">Blog Review</div>
-        </div>
-        <div className="">Profile</div>
-      </div> */}
     </div>
   );
 };
