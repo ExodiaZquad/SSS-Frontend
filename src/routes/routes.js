e123cc3fd7973e5dc70ffc119cdb92ca5ebd48bf
zantaclaus <tarzuto@gmail.com>
2022-03-26 21:12:40 +0700

    Del LoginPage.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,14 +3,12 @@ import BlogReview from '../pages/BlogReview';
 import Home from '../pages/Home';
 import ScheduleGenerator from '../pages/ScheduleGenerator';
 import SubjectFilter from '../pages/SubjectFilter';
-import Login from '../pages/LoginPage';
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login />} />
         <Route path="generator" element={<ScheduleGenerator />} />
         <Route path="filter" element={<SubjectFilter />} />
         <Route path="review" element={<BlogReview />} />
