d5b0f77ac53a5c88f3c7a2b03510496f1eb9cc9c
JusAeng <justo_play@hotmail.com>
2022-03-29 01:51:32 +0700

    justLayout

--- src/components/schedule/index.jsx
+++ src/components/schedule/index.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import './index.css'
 
 const Schedule = () => {
 
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
     return(
         <div className='sch_box-shadow'>
             <div className='sch_box'>
+                <div class="sch_headbox"></div>
+                <div class="sch_body">
+                    <div></div>
+                    <div class="sch_hourbox">
+                        {(() =>{
+                            let posts=[];
+                            for(let i=0;i<13;i++)
+                            {posts.push(<div key={i} className="sch_eachhour">{i}</div>)}
+                            return posts
+                        })()}
+                    </div>
+                </div>
+
+                {days.map((day,index) => (
+                    <div key={index} class="sch_body">
+                        <div class="sch_daybox">{day}</div>
+                        <div class="sch_barbox"></div>
+                    </div>
+                ))}
+                <div class="sch_tailbox"></div>
             </div>
         </div>
     )
--- src/pages/ScheduleGenerator/index.jsx
+++ src/pages/ScheduleGenerator/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
+import Schedule from '../../components/schedule';
 
 const ScheduleGenerator = () => {
   return (
     <div>
       <Navbar />
       <h1>Schedule Generator page.</h1>
+      <Schedule />
     </div>
   );
 };
