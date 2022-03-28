271abcdeb3ac81440b47378350ab7589c87a56d0
JusAeng <justo_play@hotmail.com>
2022-03-28 22:25:21 +0700

    createFile

---
+++ src/components/schedule/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Schedule = () => {
+
+    return(
+        <div className='sch_box-shadow'>
+            <div className='sch_box'>
+            </div>
+        </div>
+    )
+}
+
+export default Schedule;
