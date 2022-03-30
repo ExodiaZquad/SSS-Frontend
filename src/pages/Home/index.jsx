9424d5c79ab75b83188356812b9bc9790567d994
gandastik <gandastik@gmail.com>
2022-03-30 15:02:33 +0700

    add Table components and core structure

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
+import Table from '../../components/Table';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Table />
       <h1>Home page.</h1>
     </div>
   );
