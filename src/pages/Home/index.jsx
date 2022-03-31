8d8717b2c59b906e0e3a7c0d3e1a82c539740114
gandastik <gandastik@gmail.com>
2022-04-01 00:35:47 +0700

    change table to generator page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
-import Table from '../../components/Table';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <Table />
       <h1>Home page.</h1>
     </div>
   );
--- src/pages/ScheduleGenerator/index.jsx
+++ src/pages/ScheduleGenerator/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
+import Table from '../../components/Table';
 
 const ScheduleGenerator = () => {
   return (
     <div>
       <Navbar />
+      <Table />
       <h1>Schedule Generator page.</h1>
     </div>
   );
