394cee1f99e811cc27c31de62c8d7390c12d7e6d
JusAeng <justo_play@hotmail.com>
2022-04-01 20:35:45 +0700

    clear old shit

--- src/components/schedule/index.jsx
+++ src/components/schedule/index.jsx
@@ -5,43 +5,12 @@ import fake from './testData.json';
 const Schedule = () => {
 
     const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-    const bars = [[0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0]];
-    const alldate = [];
-    const allhours =[];
-    const [stay,setStay] = useState(false)
+
 
     const dateReviver = function (value) {
         return new Date(value)
     }
 
-    const rentHour= (stay) => {
-        if(stay){
-            return <div className='sch_barbox-act'></div>
-        }
-        else{
-            return <div className='sch_barbox-nact'></div>
-        }
-    }
-
-    const isToggle= () => {
-        return rentHour(stay)
-    }
-    
-    fake.map((fak) => {
-        alldate.push(dateReviver(fak.class.start).getDay());
-        alldate.push(dateReviver(fak.class.end).getDay());
-
-        allhours.push(dateReviver(fak.class.start).getHours())
-        allhours.push(dateReviver(fak.class.end).getHours())
-    })
-    // console.log(alldate);
-    console.log(allhours);
-
-    alldate.map((i, index) =>{
-        bars[i-1][allhours[index]-8] = 1 ;
-    })
-
-    console.log(bars);
 
     return(
         <div className='sch_box-shadow'>
@@ -63,9 +32,7 @@ const Schedule = () => {
                     <div key={index} className="sch_body">
                         <div className="sch_daybox">{day}</div>
                         <div className="sch_barbox">
-                            {bars[index].map((bar) => (
-                                bar ? isToggle() : rentHour(stay)
-                            ))}
+                            
 
                         </div>
                     </div>
