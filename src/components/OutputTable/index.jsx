f0a5020859c5edd45c16ae4346aad85417d580b5
gandastik <gandastik@gmail.com>
2022-04-15 14:19:51 +0700

    add styling on table and change sec style on each sec

--- src/components/OutputTable/index.jsx
+++ src/components/OutputTable/index.jsx
@@ -5,10 +5,10 @@ const OutputTable = ({ subjects }) => {
     <div className="mt-4 mb-14">
       <table className="w-full text-center font-medium ">
         <tr className="rounded">
-          <th className="border-zinc-300 p-3 bg-orange-200 text-orange-500  w-[300px]">รหัสวิชา</th>
-          <th className="border-zinc-300 p-3 bg-orange-200 text-orange-500">ชื่อวิชา</th>
-          <th className="p-3 bg-orange-200 text-orange-500">หน่วยกิต</th>
-          <th className="p-3 bg-orange-200 text-orange-500">กลุ่ม</th>
+          <th className="border-zinc-300 p-3 bg-sky-200 text-sky-600 ">รหัสวิชา</th>
+          <th className="border-zinc-300 p-3 bg-sky-200 text-sky-600">ชื่อวิชา</th>
+          <th className="p-3 bg-sky-200 text-sky-600">หน่วยกิต</th>
+          <th className="p-3 bg-sky-200 text-sky-600 w-[400px]">กลุ่ม</th>
         </tr>
         {subjects.map(subject => {
           return (
@@ -27,13 +27,22 @@ const OutputTable = ({ subjects }) => {
 };
 
 const TableRow = ({ id, name, credit, sec, hasLab }) => {
+  const colors = ['bg-green-400', 'bg-red-400', 'bg-blue-400', 'bg-orange-400'];
+
   return (
     <>
       <tr className="hover:bg-zinc-100">
         <td className="p-3 border">{id}</td>
         <td className="p-3 max-w-xs border">{name}</td>
         <td className="p-3 border">{credit}</td>
-        <td className="p-3 flex justify-center items-center border">{sec}</td>
+        <td className="p-3 flex justify-start items-center border gap-4">
+          {sec.map((secItem, index) => (
+            <div className="py-1 px-2 rounded-lg shadow-md bg-white flex items-center gap-1">
+              <div className={colors[index % 4] + ' rounded-full w-2 h-2'}></div>
+              {secItem}
+            </div>
+          ))}
+        </td>
       </tr>
     </>
   );
