b876ae37b8bce821616f0d0d72d6b4a4c8714402
zantaclaus <tarzuto@gmail.com>
2022-04-16 16:59:56 +0700

    Change label.

--- src/components/ProfileTabs/index.jsx
+++ src/components/ProfileTabs/index.jsx
@@ -25,7 +25,7 @@ const FavoriteSchedules = ({ favSchedule, getUserProfile }) => {
       {favSchedule.map((schedule, index) => (
         <>
           <div className="text-2xl mb-4 bg-[#FF837E] text-white w-fit px-5 py-2 rounded-xl shadow-md">
-            Favorite Schdule {index + 1}
+            Favorite Schedule {index + 1}
           </div>
           <Schedule data={schedule.array} onGenerate={getUserProfile} autoFill={true} />
         </>
