1dda537bdfad5a7a31ea43487fd5f144328cdb2e
gandastik <gandastik@gmail.com>
2022-04-16 16:32:08 +0700

    fetch fav schedule when click at dropdown

--- src/pages/ScheduleGenerator/index.jsx
+++ src/pages/ScheduleGenerator/index.jsx
@@ -74,9 +74,9 @@ const ScheduleGenerator = () => {
   }, [subjects]);
 
   // get fav schedule when load the page
-  useEffect(() => {
-    getFavSchedules();
-  }, []);
+  // useEffect(() => {
+  //   getFavSchedules();
+  // }, []);
 
   const updateGenerateBtn = () => {
     let totalCredit = 0;
@@ -99,6 +99,7 @@ const ScheduleGenerator = () => {
           setSubjects={setSubjects}
           setSecSelected={setSecSelected}
           setLstSubjectIdFromFav={setLstSubjectIdFromFav}
+          getFavSchedules={getFavSchedules}
         />
         <Table
           subjects={subjects}
@@ -144,6 +145,7 @@ const Dropdown = ({
   setSecSelected,
   setSubjects,
   setLstSubjectIdFromFav,
+  getFavSchedules,
 }) => {
   const [isActive, setIsActive] = useState(false);
   const options = [...dropdownOptions];
@@ -173,7 +175,10 @@ const Dropdown = ({
     <div className="select-none relative w-[250px] my-4">
       <div
         className="bg-white py-2 px-6 rounded-lg shadow-md flex justify-evenly items-center text-lg cursor-pointer"
-        onClick={e => setIsActive(!isActive)}
+        onClick={e => {
+          setIsActive(!isActive);
+          getFavSchedules();
+        }}
       >
         {selected}
         {isActive ? <VscTriangleUp color="orange" /> : <VscTriangleDown color="orange" />}
